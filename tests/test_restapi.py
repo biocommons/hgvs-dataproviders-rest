42393fe74d8f168743b007a6eae4fe39c97709e9
Caitlin Gong <clgong@vassar.edu>
2023-06-02 16:49:08 -0700

    Updated and added more tests

--- tests/test_restapi.py
+++ tests/test_restapi.py
@@ -5,147 +5,208 @@ import pytest
 base = "http://127.0.0.1:8000"
 
 @pytest.mark.skip(reason="slow")
-def test_seq():
-    #Exisiting seq
+def test_seq_e():
+    """Exisiting seq."""
     url = base + "/seq/NC_000007.13"
     assert requests.get(url).status_code == 200
     
-    #Nonexisitng seq
+def test_seq_ne():
+    """Nonexisitng seq."""
     url = base + "/seq/fake"
     assert requests.get(url).status_code == 404
     
-    #Exisiting seq with start and end indices
+def test_seq_e_indices():
+    """Exisiting seq with start and end indices."""
     url = base + "/seq/NC_000007.13?start_i=10000&end_i=10050"
     assert requests.get(url).status_code == 200
-    
-    #Exisiting with start
-    
-    #Exisitng with end
-    
-    #Nonexisiting with start and end
-    
-    #Nonexisiting with start
-    
-    #Nonexisitng with end
 
-def test_acs_for_protein_seq():
-    # Is there an ac that is associated with a short sequence?
-    # Or would there be another better way to write this test?
+#Exisiting with start
     
-    """
-    #Exisiting seq
-    seq_from_ac = requests.get("http://127.0.0.1:8000/seq/NC_000007.13").text
-    url = base + "/acs-for-protein-seq/" + seq_from_ac
-    assert "NC_000007.13" == requests.get(url).text
+#Exisitng with end
+    
+#Nonexisiting with start and end
+    
+#Nonexisiting with start
+    
+#Nonexisiting with end
+
+def test_acs_for_protein_seq_e():
+    """Exisiting seq."""
+    url = base + "/acs_for_protein_seq/MRAKWRKKRMRRLKRKRRKMRQRSK"
     assert requests.get(url).status_code == 200
+ 
+def test_acs_for_protein_seq_ne():
+    """Nonexisiting seq."""
+    url = base + "/acs_for_protein_seq/fake"
+    assert requests.get(url).status_code == 200 #Should return empty list
     
-    #Nonexisiting seq
-    url = base + "/acs-for-protein/fake"
-    assert requests.get(url).statuscode == 204
-    """
+def test_acs_for_protein_seq_ne_nonalphabetical():
+    """Non-alphabetic character seq."""
+    url = base + "/acs_for_protein_seq/123"
+    assert requests.get(url).status_code == 404
    
-def test_gene_info():
-    #Existing gene
-    url = base + "/gene-info/VHL"
+def test_gene_info_e():
+    """Existing gene."""
+    url = base + "/gene_info/VHL"
+    assert requests.get(url).status_code == 200
+
+def test_gene_info_ne():
+    """Nonexisitng gene."""
+    url = base + "/gene_info/VH"
+    assert requests.get(url).status_code == 404
+    
+def test_tx_exons_e():
+    """Exisiting seqs."""
+    url = base + "/tx_exons/NM_199425.2/NC_000020.10?alt_aln_method=splign"
     assert requests.get(url).status_code == 200
     
-    #Nonexisitng gene
-    url = base + "/gene-info/VH"
+def test_tx_exons_ne():
+    """Nonexisting seq."""
+    url = base + "/tx_exons/NM_199425.2/fake?alt_aln_method=splign"
     assert requests.get(url).status_code == 404
     
-print(requests.get(base + "/gene-info/VH").reason)
-    
-def test_tx_for_gene():
-    #Existing gene
-    url = base + "/tx-for-gene/VHL"
-    assert requests.get(url).status_code == 200
-    
-    #Nonexisitng gene
-    url = base + "/tx-for-gene/VH"
-    assert requests.get(url).status_code == 404
-    
-def test_tx_for_region():
-    #Existing region
-    url = base + "/tx-for-region/NC_000007.13?alt_aln_method=splign&start_i=0&end_i=50"
-    assert requests.get(url).status_code == 204
-    
-    #Nonexisting region
-    url = base + "/tx-for-region/fake?alt_aln_method=splign&start_i=0&end_i=50"
-    assert requests.get(url).status_code == 404
-    
-    #Exisiting region with some params
-    url = base + "/tx-for-region/NC_000007.13?alt_aln_method=splign"
+def text_tx_exons_e_params():
+    """Existing seqs with missing param"""
+    url = base + "/tx_exons/NM_199425.2/NC_000020.10"
     assert requests.get(url).status_code == 422
     
-    #Nonexisting region wtih some params
-    url = base + "/tx-for-region/fake?alt_aln_method=splign"
+def text_tx_exons_ne_params():
+    """Nonexisting seq with missing param"""
+    url = base + "/tx_exons/NM_199425.2/fake?alt_aln_method=splign"
+    assert requests.get(url).status_code == 422  #FastAPI might do this for you
+    
+def test_tx_for_gene_e():
+    """Existing gene."""
+    url = base + "/tx_for_gene/VHL"
+    assert requests.get(url).status_code == 200
+
+def test_tx_for_gene_ne():
+    """Nonexisitng gene."""
+    url = base + "/tx_for_gene/VH"
     assert requests.get(url).status_code == 404
     
-def test_alignments_for_region():
-    #Existing region
-    url = base + "/alignments-for-region/NC_000007.13?alt_aln_method=splign&start_i=0&end_i=50"
-    assert requests.get(url).status_code == 204
-    
-    #Nonexisting region
-    url = base + "/alignments-for-region/fake?alt_aln_method=splign&start_i=0&end_i=50"
-    assert requests.get(url).status_code == 404
-    
-    #Exisiting region with some params
-    url = base + "/alignments-for-region/NC_000007.13?alt_aln_method=splign"
+def test_tx_for_region_e():
+    """Existing region."""
+    url = base + "/tx_for_region/NC_000007.13?alt_aln_method=splign&start_i=0&end_i=50"
+    assert requests.get(url).status_code == 200
+
+def test_tx_for_region_ne():
+    """Nonexisting region"""
+    url = base + "/tx_for_region/fake?alt_aln_method=splign&start_i=0&end_i=50"
+    assert requests.get(url).status_code == 200 #Should return empty list
+        
+def test_tx_region_e_params():
+    """Exisiting region with only some params."""
+    url = base + "/tx_for_region/NC_000007.13?alt_aln_method=splign"
     assert requests.get(url).status_code == 422
     
-    #Nonexisting region wtih some params
-    url = base + "/alignments-for-region/fake?alt_aln_method=splign"
-    assert requests.get(url).status_code == 404
+def test_tx_region_ne_params():
+    """Nonexisting region with only some params."""
+    url = base + "/tx_for_region/fake?alt_aln_method=splign"
+    assert requests.get(url).status_code == 422 #Checks for params before checking if region exists
     
-def test_tx_identity_info():
-    #Existing transcript
-    url = base + "/tx-identity-info/NM_199425.2"
+def test_alignments_for_region_e():
+    """Existing region."""
+    url = base + "/alignments_for_region/NC_000007.13?alt_aln_method=splign&start_i=0&end_i=50"
     assert requests.get(url).status_code == 200
     
-    #Nonexisting transcript
-    url = base + "/tx-identity-info/fake"
-    assert requests.get(url).status_code == 404
+def test_alignments_for_region_ne():
+    """Nonexisting region."""
+    url = base + "/alignments_for_region/fake?alt_aln_method=splign&start_i=0&end_i=50"
+    assert requests.get(url).status_code == 200 #Empty list 
     
-def test_tx_mapping_options():
-    #Existing transcript
-    url = base + "/tx-mapping-options/NM_000051.3"
+def test_alignmemts_for_region_e_params():
+    """Exisiting region with only some params."""
+    url = base + "/alignments_for_region/NC_000007.13?alt_aln_method=splign"
+    assert requests.get(url).status_code == 422
+    
+def test_alignments_for_region_ne_params():
+    """Nonexisting region with only some params."""
+    url = base + "/alignments_for_region/fake?alt_aln_method=splign"
+    assert requests.get(url).status_code == 422 #Checks for params before checking if region exists
+    
+def test_tx_identity_info_e():
+    """Existing transcript."""
+    url = base + "/tx_identity_info/NM_199425.2"
     assert requests.get(url).status_code == 200
     
-    #Existing transcripts no alignments
-    url = base + "/tx-mapping-options/NC_000007.13"
-    assert requests.get(url).status_code == 204
-    
-    #Nonexisting transcript
-    url = base + "/tx-mapping-options/fake"
+def test_tx_identity_info_ne():
+    """Nonexisting transcript."""
+    url = base + "/tx_identity_info/fake"
     assert requests.get(url).status_code == 404
     
-def test_similar_transcripts():
-    #Existing transcript
-    url = base + "/similar-transcripts/NM_000051.3"
+def test_tx_info_e():
+    """Existing seqs."""
+    url = base + "/tx_info/NM_199425.2/NC_000020.10?alt_aln_method=splign"
     assert requests.get(url).status_code == 200
     
-    #Existing transcripts no similar transcripts
-    url = base + "/similar-transcripts/NC_000007.13"
-    assert requests.get(url).status_code == 204
-    
-    #Nonexisting transcript
-    url = base + "/similar-transcripts/fake"
+def test_tx_info_ne():
+    """Nonexisting seq."""
+    url = base + "/tx_info/NM_199425.2/fake?alt_aln_method=splign"
     assert requests.get(url).status_code == 404
     
-def test_pro_ac_for_tx_ac():
-    #Existing transcript
-    url = base + "/pro-ac-for-tx-ac/NM_000051.3"
+def test_tx_info_e_param():
+    """Existing seqs."""
+    url = base + "/tx_info/NM_199425.2/NC_000020.10"
+    assert requests.get(url).status_code == 422
+    
+def test_tx_info_ne_param():
+    """Nonexsiting seq."""
+    url = base + "/tx_info/NM_199425.2/fake"
+    assert requests.get(url).status_code == 422
+    
+def test_tx_mapping_options_e():
+    """Existing transcript."""
+    url = base + "/tx_mapping_options/NM_000051.3"
     assert requests.get(url).status_code == 200
     
-    #Existing transcripts no associated proteins
-    url = base + "/pro-ac-for-tx-ac/NC_000007.13"
-    assert requests.get(url).status_code == 204
+def test_tx_mapping_options_e_none():
+    """Existing transcripts no alignments."""
+    url = base + "/tx_mapping_options/NM_..." #Is there an example of a tx_ac with no alginments? (Should 204 even be a possible error code?)
+    assert requests.get(url).status_code == 200 #Empty list
     
-    #Nonexisting transcript
-    url = base + "/pro-ac-for-tx-ac/fake"
+def test_tx_mapping_options_ne():
+    """Nonexisting transcript."""
+    url = base + "/tx_mapping_options/fake"
+    assert requests.get(url).status_code == 200 #Empty list
+    
+def test_similar_transcripts_e():
+    """Existing transcript."""
+    url = base + "/similar_transcripts/NM_000051.3"
+    assert requests.get(url).status_code == 200
+    
+def test_similar_transcripts_e_none():
+    """Existing transcripts no similar transcripts."""
+    url = base + "/similar_transcripts/NM_..." #Need example
+    assert requests.get(url).status_code == 200 #Empty list
+    
+def test_similar_transcripts_ne():
+    """Nonexisting transcript."""
+    url = base + "/similar_transcripts/fake"
+    assert requests.get(url).status_code == 200 #Empty list
+    
+def test_pro_ac_for_tx_ac_e():
+    """Existing transcript."""
+    url = base + "/pro_ac_for_tx_ac/NM_000051.3"
+    assert requests.get(url).status_code == 200
+    
+def test_pro_ac_for_tx_ac_e_none():
+    """Existing transcripts no associated proteins."""
+    url = base + "/pro_ac_for_tx_ac/NM_..." #Need example
+    assert requests.get(url).status_code == 200 #Empty list
+    
+def test_pro_ac_for_tx_ac_ne():
+    """Nonexisting transcript."""
+    url = base + "/pro_ac_for_tx_ac/fake"
+    assert requests.get(url).status_code == 200 #Empty list 
+    
+def test_assembly_map_e():
+    """Existing assembly name."""
+    url = base + "/assembly_map/GRCh38.p5"
+    assert requests.get(url).status_code == 200
+    
+def test_assembly_map_ne():
+    """Nonexisting assembly name."""
+    url = base + "/assembly_map/GROUCH"
     assert requests.get(url).status_code == 404
     
-def test_assembly_map():
-    url = base + "/assembly-map/GRCh38.p5"
-    assert requests.get(url).status_code == 200
